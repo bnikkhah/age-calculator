333cfd3f270b46a4d335249524d3727708dcbcaa
Behnam Nikkhah <bnikkhah22@gmail.com>
2023-05-11 22:08:57 -0600

    add fields

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 import "./style.scss";
-
 import Alpine from "alpinejs";
-
 window.Alpine = Alpine;
+
+document.addEventListener("alpine:init", () => {
+  Alpine.data("data", () => ({
+    day: 0,
+    month: 0,
+    year: 0,
+    handleSubmit(e) {},
+  }));
+});
 
 Alpine.start();

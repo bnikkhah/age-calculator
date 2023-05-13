43af0689e054164be6adfd9a89e5b60ddfef6a57
Behnam Nikkhah <bnikkhah22@gmail.com>
2023-05-12 20:47:03 -0600

    age calculation with moment

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 import "./style.scss";
 import Alpine from "alpinejs";
+import moment from "moment";
 window.Alpine = Alpine;
 
 document.addEventListener("alpine:init", () => {
   Alpine.data("data", () => ({
-    day: 0,
-    month: 0,
-    year: 0,
-    handleSubmit(e) {},
+    day: "",
+    month: "",
+    year: "",
+    calculatedDays: "--",
+    calculatedMonths: "--",
+    calculatedYears: "--",
+    handleSubmit(e) {
+      const duration = moment.duration(
+        moment().diff(`${this.year}-${this.month}-${this.day}`)
+      );
+
+      this.calculatedDays = duration.days();
+      this.calculatedMonths = duration.months();
+      this.calculatedYears = duration.years();
+    },
   }));
 });
 
